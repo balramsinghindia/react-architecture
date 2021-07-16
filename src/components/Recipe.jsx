6dfeb9554f5733b4a04b74bb561b679515207199
balramakqa <balram.singh@akqa.com>
2021-07-16 21:44:41 +1000

    Added first state in React architecture

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
+let technologies = {
+    frontend: ['js', 'css', 'html', 'typescript'],
+    backend: ['java', 'php']
+}
+
+let receipe = {
+    ...technologies,
+    leather: 1,
+    refined: 'hello',
+    welcome: true
+}
+
 const Recipe = ({text}) => {
+    const [abc, setAbc] = useState({});
+
     return (
         <div>
             {text}
+            <h3>Current Recipe:</h3>
+            <button onClick={()=>setAbc(technologies)}>Recipes</button>
+            <button onClick={()=>setAbc(receipe)}>Technologies</button>
+            <ul>
+                { Object.keys(abc).map((material) => <li>{material}: {abc[material]}</li> )}
+            </ul>
+            <Header />
+            
         </div>
     )
 }
