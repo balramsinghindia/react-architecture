a94965710a9791960efbf3f87e8b35eb73d952fa
balramakqa <balram.singh@akqa.com>
2021-07-29 21:15:18 +1000

    Added images as data base 64 format

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif|svg)$/i,
-                type: 'asset/resource',
+                type: 'asset/inline',
             }
         ]
     },
