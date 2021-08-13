8480b256c1d180e0fa8d1bbdb46303e3c0bbfb23
balramakqa <balram.singh@akqa.com>
2021-08-13 21:51:11 +1000

    Added redux with react hooks in react architecture

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,7 +1,25 @@
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
 
 
 export default function About(){
+
+    const counter = useSelector(state => state);
+
+    const dispatch = useDispatch();
+
+    const data = {
+        response: {
+            "english": 23,
+            "hindi": 21,
+            "dataStructure": 12
+        }
+    }
+
+
+
     useEffect(()=> {
         window.scroll(0, 0);
     }, []);
@@ -9,6 +27,26 @@ export default function About(){
     return (
         <>
             <h3>About Page</h3>
+            {counter.vehicle}
+
+            <button
+                onClick={() =>
+                dispatch({
+                    type: "Car",
+                    data: data
+                })
+                }
+            >Car</button>
+
+            <button
+                onClick={() =>
+                dispatch({
+                    type: "Bike",
+                    data: data
+
+                })
+                }
+            >Bike</button>
             <div className="about">
                 hello i am about
             </div>
